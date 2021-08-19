cb6108ba2a5df6cc5672ed96e19da8dec0f07c59
Joe Morin <jmorin@unfi.com>
2021-08-19 20:12:46 +0000

    edit s3 bucket module, prefix

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/joemorin-training/s3-bucket/aws"
   version = "2.2.0"
-  bucket_prefix = "JoeMorin"
+  bucket_prefix = "joemorin"
    # insert required variables here
 }
